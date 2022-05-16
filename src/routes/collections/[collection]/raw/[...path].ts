c9505b02ac400d5ec934fb1d407c21776916d0a6
Phoenix Fox <Bluebie@users.noreply.github.com>
2022-05-16 19:48:31 +1000

    do better 404s?

--- src/routes/collections/[collection]/raw/[...path].ts
+++ src/routes/collections/[collection]/raw/[...path].ts
@@ -6,84 +6,92 @@ import { decodeCollectionURLPath } from '$lib/functions/collection-url'
 import { isValid, isAuthorized } from '../_auth'
 
 export const get: RequestHandler = async function ({ request }) {
-  const params = decodeCollectionURLPath((new URL(request.url)).pathname)
-  const { collection, path } = params
-  const dataPath = `collections/${collection}/${path}`
+    try {
+    const params = decodeCollectionURLPath((new URL(request.url)).pathname)
+    const { collection, path } = params
+    const dataPath = `collections/${collection}/${path}`
 
-  // validations
-  if (!isValid(params)) return { status: 500 }
-  if (!isWithin(dataPath, `collections/${collection}`)) return { status: 500 }
-  if (!await isAuthorized(params, request)) return { status: 307, headers: { Location: '/identity/login' } }
+    // validations
+    if (!isValid(params)) return { status: 500 }
+    if (!isWithin(dataPath, `collections/${collection}`)) return { status: 500 }
+    if (!await isAuthorized(params, request)) return { status: 307, headers: { Location: '/identity/login' } }
 
-  const stats = await getInfo(dataPath)
+    const stats = await getInfo(dataPath)
 
-  if (stats.isFile) {
-    const isText = stats.type.startsWith('text/') || stats.type.split(/[^a-z0-9]+/ig).includes('json')
-    const headers = {
-      'Accept-Ranges': 'bytes',
-      'Content-Type': isText ? `${stats.type}; charset=utf-8` : stats.type,
-      'Content-Length': `${stats.size}`,
-      'Last-Modified': stats.lastModified.toUTCString(),
-      'Etag': stats.etag,
-    }
+    if (stats.isFile) {
+      const isText = stats.type.startsWith('text/') || stats.type.split(/[^a-z0-9]+/ig).includes('json')
+      const headers = {
+        'Accept-Ranges': 'bytes',
+        'Content-Type': isText ? `${stats.type}; charset=utf-8` : stats.type,
+        'Content-Length': `${stats.size}`,
+        'Last-Modified': stats.lastModified.toUTCString(),
+        'Etag': stats.etag,
+      }
 
-    if (request.headers.has('Range')) {
-      // range request
-      const rangesStr = request.headers.get('Range').split('bytes=')[1]
-      const ranges = rangesStr.split(',').map(x => x.trim().split('-', 2).map(x => Number(x)))
-      if (ranges.length === 1) {
-        // requesting a single range
-        return {
-          status: 206,
-          headers: {
-            ...headers,
-            'Content-Range': `bytes ${ranges[0].join('-')}/${headers['Content-Length']}`,
-            'Content-Length': `${ranges[0][1] - ranges[0][0]}`
-          },
-          body: (await read(dataPath)).subarray(...ranges[0])
-        }
-      } else if (ranges.length > 1) {
-        // multipart response
-        const boundary = nanoid()
-        const data = await read(dataPath)
-        const bodies = []
-        const t = stringToByteArray
+      if (request.headers.has('Range')) {
+        // range request
+        const rangesStr = request.headers.get('Range').split('bytes=')[1]
+        const ranges = rangesStr.split(',').map(x => x.trim().split('-', 2).map(x => Number(x)))
+        if (ranges.length === 1) {
+          // requesting a single range
+          return {
+            status: 206,
+            headers: {
+              ...headers,
+              'Content-Range': `bytes ${ranges[0].join('-')}/${headers['Content-Length']}`,
+              'Content-Length': `${ranges[0][1] - ranges[0][0]}`
+            },
+            body: (await read(dataPath)).subarray(...ranges[0])
+          }
+        } else if (ranges.length > 1) {
+          // multipart response
+          const boundary = nanoid()
+          const data = await read(dataPath)
+          const bodies = []
+          const t = stringToByteArray
 
-        for (const range of ranges) {
-          bodies.push(t(`--${boundary}\r\n`))
-          bodies.push(t(`Content-Type: ${headers['Content-Type']}\r\n`))
-          bodies.push(t(`Content-Range: ${range[0]}-${range[1]}/${headers['Content-Length']}\r\n`))
-          bodies.push(t(`\r\n`))
-          bodies.push(data.subarray(range[0], range[1]))
-        }
+          for (const range of ranges) {
+            bodies.push(t(`--${boundary}\r\n`))
+            bodies.push(t(`Content-Type: ${headers['Content-Type']}\r\n`))
+            bodies.push(t(`Content-Range: ${range[0]}-${range[1]}/${headers['Content-Length']}\r\n`))
+            bodies.push(t(`\r\n`))
+            bodies.push(data.subarray(range[0], range[1]))
+          }
 
-        const byteIter = function * () {
-          for (const section of bodies) yield * section
-        }
-        const body = new Uint8Array(byteIter())
-        return {
-          status: 206,
-          headers: {
-            ...headers,
-            'Content-Type': `multipart/byteranges; boundary=${boundary}`,
-            'Content-Length': `${body.byteLength}`
-          },
-          body: body
+          const byteIter = function * () {
+            for (const section of bodies) yield * section
+          }
+          const body = new Uint8Array(byteIter())
+          return {
+            status: 206,
+            headers: {
+              ...headers,
+              'Content-Type': `multipart/byteranges; boundary=${boundary}`,
+              'Content-Length': `${body.byteLength}`
+            },
+            body: body
+          }
         }
       }
-    }
-    // return a normal request
-    return { headers, body: await read(dataPath) }
-  } else {
-    if (request.headers.get('Accept') === 'text/plain') {
-      const body = (await listStrings(dataPath)).join('\n')
-      return { headers: { 'Content-Type': 'text/plain' }, body }
+      // return a normal request
+      return { headers, body: await read(dataPath) }
     } else {
-      return {
-        body: {
-          files: await list(dataPath)
+      if (request.headers.get('Accept') === 'text/plain') {
+        const body = (await listStrings(dataPath)).join('\n')
+        return { headers: { 'Content-Type': 'text/plain' }, body }
+      } else {
+        return {
+          body: {
+            files: await list(dataPath)
+          }
         }
       }
+    }
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      return { status: 404 }
+    } else {
+      throw err
     }
   }
 }
